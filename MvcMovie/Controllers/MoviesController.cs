4883650b28314a20863b7b0ad172a508086dea0c
Unknown <krunalbodiwala7311@icloud.com>
2018-05-16 18:06:35 +1200

    Added Search by Genre
    Populated option field in the form with list of genres(distinct).
    Users can now select a genre as search filter

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -19,17 +19,39 @@ namespace MvcMovie.Controllers
         }
         //Using LINQ to perform search. 
         //SQLite is case sensitive. 
-        public async Task<IActionResult> Index(string searchString)
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
         {
+          
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
             var movies = from m in _context.Movie
                          select m;
 
+            //Search by title
             if (!String.IsNullOrEmpty(searchString))
             {
                 movies = movies.Where(s => s.Title.Contains(searchString));
             }
 
-            return View(await movies.ToListAsync());
+            //Search by Genre
+            if (!String.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+
+            var movieGenreVM = new MovieGenreViewModel();
+
+            //Select distinct genres
+            movieGenreVM.genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            movieGenreVM.movies = await movies.ToListAsync();
+
+            return View(movieGenreVM);
+
+
         }
         // GET: Movies/Details/5
         public async Task<IActionResult> Details(int? id)
---
+++ MvcMovie/Models/MovieGenreViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+namespace MvcMovie.Models
+{
+    public class MovieGenreViewModel
+    {
+        //List of Movies
+        public List<Movie> movies;
+
+        //List of Genres
+        public SelectList genres;
+
+        //Selected Movie Genre
+        public string movieGenre { get; set; }
+    }
+}
